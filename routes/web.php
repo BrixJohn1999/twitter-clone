e90a4beede15eb9dcfd9a1f660be7576d91f40f1
Brix John Novesteras <brixjohnovesteras@gmail.com>
2024-04-09 15:13:41 +0800

    layout

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/terms', function () {
+    return view('terms');
+});
 
 Route::get('/profile', [ProfileController::class, 'index']);
